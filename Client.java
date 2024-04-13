00d0e1410f01d6e45cfbee09425407a28e72f4d0
Vincent Gruse <grusvin10@gmail.com>
2024-04-13 16:02:29 -0400

    initial commit, added timeout, simplified a few areas

--- Client.java
+++ Client.java
@@ -1,43 +1,41 @@
 /**
  * JAVA SOCKET CLIENT
- * 
+ *
  * @author Aastha Bhatt
  * @author Matthew Dibbern
  * @author Vincent Gruse
  * @author Leo Tangban
  * @author Emmanuel Taylor
- * 
+ *
  * @description
  *    This class represents a Java UDP Client that sends a request to a server containing a web server
  *    and timer value. The client receives data from server and sends exactly one ACK to the server.
- * 
+ *
  * @packages
  *    Java IO (BufferedReader, ByteArrayOutputStream, IOException, InputStreamReader)
  *    Java Net (DatagramPacket, DatagramSocket, InetAddress)
  *    Java NIO Charset (StandardCharsets)
  */
 
- import java.io.BufferedReader;
- import java.io.ByteArrayOutputStream;
- import java.io.IOException;
- import java.io.InputStreamReader;
- import java.net.DatagramPacket;
- import java.net.DatagramSocket;
- import java.net.InetAddress;
- import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
-    private final static int PORT = 11111;
+    private static final int SERVER_PORT = 11111;
+    private static final int ACK_PORT = SERVER_PORT + 1;
+    private static final int TIMEOUT = 5000; // Timeout in milliseconds
 
     public static void main(String[] args) {
-        DatagramSocket clientSocket = null;
-
         System.out.println("\n==================================================================================");
         System.out.println("=========================JAVA SOCKET APPLICATION - CLIENT=========================");
         System.out.println("==================================================================================\n");
 
-        try {
-            clientSocket = new DatagramSocket();
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
             // Collect user input.
@@ -46,61 +44,51 @@ public class Client {
             System.out.print("Enter timer value in seconds: ");
             int timerValue = Integer.parseInt(userInput.readLine());
 
-            // Start timer.
-            long startTime = System.currentTimeMillis();
-
             // Send the client request to the server.
-            String clientRequest = webServerName + "," + timerValue;
-            byte[] sendData = clientRequest.getBytes();
             InetAddress serverAddress = InetAddress.getLocalHost();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, PORT);
-            clientSocket.send(sendPacket);
+            sendRequest(clientSocket, webServerName, timerValue, serverAddress);
 
-            // Retrieve data packets from the server.
-            byte[] receiveData = new byte[1024];
-            ByteArrayOutputStream receivedDataBuffer = new ByteArrayOutputStream();
+            // Receive and process data from the server.
+            String receivedData = receiveData(clientSocket, serverAddress);
 
-            while (true) {
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length, serverAddress, PORT);
-                clientSocket.receive(receivePacket);
-                String receiveMessage = new String(receivePacket.getData(), 0, receivePacket.getLength(), StandardCharsets.UTF_8);
-                
-                String[] parts = receiveMessage.split("xxx");
-                receivedDataBuffer.write(parts[1].getBytes(), 0, parts[1].length());
-
-                String[] headers = parts[0].split(",");
-                if (Integer.parseInt(headers[0]) == Integer.parseInt(headers[1]) - 1) {
-                    break;
-                }
-            }
-            
-            // Close ClientSocket.
-            clientSocket.close();
-            
-            // Stop timer.
-            long elapsedTime = System.currentTimeMillis() - startTime;
-
-            // If the packets were recieved within the time limit, print the contents,
-            // and send an ACK to the server. Otherwise, print FAIL.
-            if (elapsedTime < timerValue * 1000) {
-                DatagramSocket ackSocket = new DatagramSocket();
-                String ackMessage = "ACK";
-                byte[] ackData = ackMessage.getBytes();
-                DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length, serverAddress, PORT + 1);
-                ackSocket.send(ackPacket);
-                String recievedData = receivedDataBuffer.toString();
-                System.out.println("\n" + recievedData + "\n");
+            // Print received data and send ACK if the operation was successful.
+            if (receivedData != null) {
+                System.out.println("\n" + receivedData + "\n");
+                sendACK(clientSocket, serverAddress);
                 System.out.println("OK");
-                ackSocket.close();
             } else {
                 System.out.println("\nFAIL");
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (clientSocket != null && !clientSocket.isClosed()) {
-                clientSocket.close(); // Close the serverSocket in the finally block
-            }
         }
+    }
+
+    private static void sendRequest(DatagramSocket socket, String webServerName, int timerValue, InetAddress serverAddress) throws IOException {
+        String clientRequest = webServerName + "," + timerValue;
+        byte[] sendData = clientRequest.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, SERVER_PORT);
+        socket.send(sendPacket);
+    }
+
+    private static String receiveData(DatagramSocket socket, InetAddress serverAddress) throws IOException {
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        socket.setSoTimeout(TIMEOUT); // Set timeout for receiving data
+        try {
+            socket.receive(receivePacket);
+            String receiveMessage = new String(receivePacket.getData(), 0, receivePacket.getLength(), StandardCharsets.UTF_8);
+            String[] parts = receiveMessage.split("xxx");
+            return parts[1];
+        } catch (IOException e) {
+            return null; // Timeout occurred
+        }
+    }
+
+    private static void sendACK(DatagramSocket socket, InetAddress serverAddress) throws IOException {
+        String ackMessage = "ACK";
+        byte[] ackData = ackMessage.getBytes();
+        DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length, serverAddress, ACK_PORT);
+        socket.send(ackPacket);
     }
 }
